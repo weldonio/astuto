372843b430d9e7474286c06c0b30dad77ef23c12
Aleksey Tkachenko <atkachenko@weldon.io>
2021-05-17 10:12:13 +0000

    E-mails sending and SSL handling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -62,6 +62,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # SMTP Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp-relay.gmail.com',
+    port:                 587,
+    domain:               'smtp-relay.gmail.com'
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
